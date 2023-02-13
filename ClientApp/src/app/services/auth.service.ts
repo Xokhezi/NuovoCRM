c0fcd36bbe7f5e94b84c2715a87be79fb6e69d65
Xokhezi <110103053+Xokhezi@users.noreply.github.com>
2023-02-13 13:35:39 +0100

    ng jwt service+loginpage

---
+++ ClientApp/src/app/services/auth.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http:HttpClient) { }
+
+  login(credentials:any)
+  {
+    return this.http.post('https://localhost:7011/api/auth',credentials)
+    .subscribe((r:any)=>localStorage.setItem('token',r.token));
+  }
+  logout() { 
+    localStorage.removeItem('token');
+  }
+
+  isLoggedIn() { 
+    
+    let jwtHelper = new JwtHelperService();
+    let token = localStorage.getItem('token');
+
+    if(!token)
+      return false;
+
+    let expDate= jwtHelper.getTokenExpirationDate(token);
+    let isExp = jwtHelper.isTokenExpired(token);  
+    
+    
+    return !isExp;
+  }
+}
