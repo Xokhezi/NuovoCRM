afb3198ff7aa3324cc2a3c3ba2a03131a60d564e
Xokhezi <110103053+Xokhezi@users.noreply.github.com>
2023-02-14 08:57:37 +0100

    ng validation implemented

--- ClientApp/src/app/services/partners.service.ts
+++ ClientApp/src/app/services/partners.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -7,20 +7,29 @@ import { Injectable } from '@angular/core';
 export class PartnersService {
 
   constructor(private http:HttpClient) { }
+  getValidation()
+  {
+    let headers=new HttpHeaders();
+    let token = localStorage.getItem('token');
+    headers = headers.append('Authorization', 'Bearer '+ token);
+    let options = {headers:headers};
+
+    return options;
+  }
   GetPartners()
   {
-    return this.http.get('https://localhost:7011/api/partners');
+    return this.http.get('https://localhost:7011/api/partners',this.getValidation());
   }
   GetPartner(id:any)
   {
-    return this.http.get('https://localhost:7011/api/partners/'+id);
+    return this.http.get('https://localhost:7011/api/partners/'+id,this.getValidation());
   }
   DeletePartner(id:any)
   {
-    return this.http.delete('https://localhost:7011/api/partners/'+id);
+    return this.http.delete('https://localhost:7011/api/partners/'+id,this.getValidation());
   }
   CreatePartner(partner:any)
   {
-    return this.http.post('https://localhost:7011/api/partners', partner);
+    return this.http.post('https://localhost:7011/api/partners', partner,this.getValidation());
   }
 }
--- ClientApp/src/app/services/products.service.ts
+++ ClientApp/src/app/services/products.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -7,24 +7,33 @@ import { Injectable } from '@angular/core';
 export class ProductsService {
 
   constructor(private http:HttpClient) { }
+  getValidation()
+  {
+    let headers=new HttpHeaders();
+    let token = localStorage.getItem('token');
+    headers = headers.append('Authorization', 'Bearer '+ token);
+    let options = {headers:headers};
+
+    return options;
+  }
   GetProducts()
   {
-    return this.http.get('https://localhost:7011/api/products');
+    return this.http.get('https://localhost:7011/api/products',this.getValidation());
   }
   GetProduct(id:any)
   {
-    return this.http.get('https://localhost:7011/api/products/'+id);
+    return this.http.get('https://localhost:7011/api/products/'+id,this.getValidation());
   }
   DeleteProduct(id:any)
   {
-    return this.http.delete('https://localhost:7011/api/products/'+id);
+    return this.http.delete('https://localhost:7011/api/products/'+id,this.getValidation());
   }
   CreateProduct(product:any)
   {
-    return this.http.post('https://localhost:7011/api/products', product);
+    return this.http.post('https://localhost:7011/api/products', product,this.getValidation());
   }
   UpdateProduct(product:any,id:any)
   {
-    return this.http.put('https://localhost:7011/api/products/'+id, product);
+    return this.http.put('https://localhost:7011/api/products/'+id, product,this.getValidation());
   }
 }
