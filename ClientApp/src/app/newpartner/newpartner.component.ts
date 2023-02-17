e6e11f818f623e6cf9a4cd1d90a02ac7b04f61b0
Xokhezi <nemec.nuovo@gmail.com>
2023-02-17 12:02:00 +0100

    backend user creation

--- ClientApp/src/app/newpartner/newpartner.component.ts
+++ ClientApp/src/app/newpartner/newpartner.component.ts
@@ -1,3 +1,4 @@
+import { UsersService } from './../services/users.service';
 import { PartnersService } from './../services/partners.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -10,10 +11,11 @@ import { elementAt } from 'rxjs';
 })
 export class NewpartnerComponent implements OnInit {
   complete = false;
-  hover=false;
+  hover = false;
+  invalidPasswords=false;
   partners: any;
-  id:any;
-  isPartner:any;
+  id: any;
+  isPartner: any;
   partner = {
     id: 0,
     name: "",
@@ -38,7 +40,7 @@ export class NewpartnerComponent implements OnInit {
     passwordRepeat: ""
   };
 
-  constructor(private active: ActivatedRoute, private partnerService: PartnersService, private router: Router) { }
+  constructor(private active: ActivatedRoute, private partnerService: PartnersService, private router: Router, private userService: UsersService) { }
 
   ngOnInit(): void {
     this.partnerService.GetPartners().subscribe(p => this.partners = p);
@@ -46,26 +48,41 @@ export class NewpartnerComponent implements OnInit {
       this.id = params.get('id?');
       if (this.id != 0) {
         this.partnerService.GetPartner(this.id)
-          .subscribe((p:any) =>this.partner = p);
+          .subscribe((p: any) => this.partner = p);
       }
     })
 
   }
   submit(f: any) {
-    if(this.id==0){
-    this.partnerService.GetPartner(this.partner.leadId)
-      .subscribe(({level, teamId}: any) => {
-        this.partner.level = level + 1;
-        this.partner.teamId = level === 0 ?
-          Math.max(...this.partners.map(({teamId}: any) => teamId)) + 1 :
-          teamId;
+    if (this.id == 0) {
+      this.partnerService.GetPartner(this.partner.leadId)
+        .subscribe(({ level, teamId }: any) => {
+          this.partner.level = level + 1;
+          this.partner.teamId = level === 0 ?
+            Math.max(...this.partners.map(({ teamId }: any) => teamId)) + 1 :
+            teamId;
 
-        this.partnerService.CreatePartner(this.partner)
-          .subscribe(() => this.router.navigate(['/partners']));
-      });
+          this.partnerService.CreatePartner(this.partner)
+            .subscribe(() => this.router.navigate(['/partners']));
+        });
+      if (this.isPartner&&this.user.password==this.user.passwordRepeat) {
+        let userToCreate = {
+          email: this.partner.email,
+          fullName: this.partner.name+this.partner.surname,
+          password: this.partner.email,
+          role: this.user.role,
+          country: this.partner.adress,
+          adress: this.partner.adress,
+          phone: this.partner.phone
+        };
+        this.userService.CreateUser(userToCreate)
+        .subscribe(r=>console.log(r));       
+      }
+      else
+      this.invalidPasswords=true;
     }
-    else 
-      this.partnerService.UpdatePartner(this.partner,this.id).subscribe(r=>this.router.navigate(['/partners']));
+    else
+      this.partnerService.UpdatePartner(this.partner, this.id).subscribe(r => this.router.navigate(['/partners']));
   }
   switch() {
     this.hover = !this.hover;
