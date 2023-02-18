7a912762b1eb5fafa8f4eb599544bafb3e58c8b9
Xokhezi <nemec.nuovo@gmail.com>
2023-02-18 13:47:17 +0100

    update

--- ClientApp/src/app/animations/animations.ts
+++ ClientApp/src/app/animations/animations.ts
@@ -11,9 +11,42 @@ export let listAnim=trigger('listAnim', [
         ])
       ])
   ])
+  export let cart=trigger('cart', [
+    transition(':enter', [
+        query('.cart-item', [
+          style({opacity: 0, transform: 'translateY(-10px)'}),
+          stagger(200, [
+            animate('500ms cubic-bezier(0.35, 0, 0.25, 1)',
+            style({ opacity: 1, transform: 'none' }))
+          ])
+        ])
+      ]),
+      transition(':leave', [
+        query('.cart-item', [
+          style({opacity: 1, transform: 'translateY(10px)'}),
+          stagger(200, [
+            animate('500ms cubic-bezier(0.35, 0, 0.25, 1)',
+            style({ opacity: 0, transform: 'none' }))
+          ])
+        ])
+      ])
+  ]);
   export let fadeIn=trigger('fadeIn', [
     transition(':enter', [
         style({ opacity: 0, transform: 'translateY(10px)' }),
         animate('500ms ease-out')
     ]),
-  ])
+  ]);
+  export let fadeleft=trigger('fadeleft', [
+    transition(':leave', [
+      animate('500ms ease-out', style({ opacity: 0, transform: 'translateX(-100px)' }))
+    ])    
+  ]);
+  export let faderight=trigger('faderight',  [    
+    transition(':enter', [
+      animate('1000ms 500ms ease-out', style({ opacity: 1, transform: 'translateX(10px)' }))
+    ]),
+
+])
+
+ 
--- ClientApp/src/app/shop/shop.component.ts
+++ ClientApp/src/app/shop/shop.component.ts
@@ -1,10 +1,17 @@
+import { fadeleft, faderight } from './../animations/animations';
 import { ProductsService } from './../services/products.service';
 import { Component } from '@angular/core';
+import { cart } from '../animations/animations';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.css']
+  styleUrls: ['./shop.component.css'],
+  animations: [
+    cart,
+    fadeleft,
+    faderight
+  ]
 })
 export class ShopComponent {
   products: any;
@@ -12,10 +19,27 @@ export class ShopComponent {
   expanded = false;
   hover = false;
   cart: any;
+  totalPrize: any;
+  toOrder=false;
+  partner = {
+    id: 0,
+    name: "",
+    surname: "",
+    email: "",
+    phone: "",
+    position: "",
+    adress: "",
+    leadId: 0,
+    level: 0,
+    teamId: 0,
+    country: "",
+    userId: 0
+  };
 
   constructor(private productService: ProductsService) { }
 
   ngOnInit(): void {
+    this.cart = [];
     this.productService.GetProducts().subscribe((p: any) => {
       this.products = p.map((pr: any) => {
         return {
@@ -27,8 +51,13 @@ export class ShopComponent {
     });
   }
   search(input: string) {
-    this.productService.GetProducts().subscribe(p => {
-      this.products = p;
+    this.productService.GetProducts().subscribe((p: any) => {
+      this.products = p.map((pr: any) => {
+        return {
+          ...pr,
+          count: 1
+        }
+      })
       const inputUp = input.toUpperCase();
       this.products = this.products.filter((n: any) => n.name.toUpperCase().includes(inputUp));
     });
@@ -48,8 +77,28 @@ export class ShopComponent {
     else
       return product.count;
   }
-  addToCart(product:any)
-  {
-
+  addToCart(product: any) {
+    this.cart.push(product);
+    this.totalPrize = this.cart.reduce((ac: any, currentPrize: { prize: any; count: any }) => {
+      if (currentPrize.count! < 1)
+        return ac + currentPrize.prize * currentPrize.count;
+      else
+        return ac + currentPrize.prize
+    }, 0);
+    console.log(this.totalPrize);
   }
+  summarize() {
+    this.totalPrize = this.cart.reduce((ac: any, currentPrize: { prize: any; count: any }) => {
+      if (currentPrize.count! > 1)
+        return ac + currentPrize.prize * currentPrize.count;
+      else
+        return ac + currentPrize.prize
+    }, 0);
+    console.log(this.totalPrize)
+  }
+  toOrderForm()
+  {
+    this.toOrder=!this.toOrder
+  }
+  createOrder(f:any) { }
 }
