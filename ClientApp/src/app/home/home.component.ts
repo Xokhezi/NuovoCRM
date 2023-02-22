61f58778ebfb7a8c09b5edab14e70140ad688bd3
Xokhezi <110103053+Xokhezi@users.noreply.github.com>
2023-02-22 12:28:18 +0100

    page and redesign

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { fadeIn } from './../animations/animations';
 import { AuthService } from './../services/auth.service';
 import { Component } from '@angular/core';
@@ -13,9 +14,16 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   user = { Role: '' };
 
-  constructor(public service: AuthService) { }
+  constructor(public service: AuthService,private route:Router) { }
   ngOnInit(): void {
     this.user = this.service.getcurrentUser();
   }
-
+  toShop()
+  {
+    this.route.navigate(['shop']);
+  }
+  toOrders()
+  {
+    this.route.navigate(['orders/my']);
+  }
 }
