da65060168f170f0fea3b520a86c56fe23e84dce
Xokhezi <110103053+Xokhezi@users.noreply.github.com>
2023-02-23 07:43:04 +0100

    description implemented

--- ClientApp/src/app/new-product/new-product.component.ts
+++ ClientApp/src/app/new-product/new-product.component.ts
@@ -1,6 +1,8 @@
+import { CategoriesService } from './../services/categories.service';
 import { ProductsService } from './../services/products.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component } from '@angular/core';
+import { any } from 'underscore';
 
 @Component({
   selector: 'app-new-product',
@@ -8,36 +10,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-product.component.css']
 })
 export class NewProductComponent {
+  hover = false;
+
   id: any;
-  hover=false;
-  product={
-    name:"",
-    productCode:"",
-    prize:0,
-    discount:0,
-    discountStep:0,
-    url:""
+
+  product = {
+    id: 0,
+    name: "",
+    productCode: "",
+    prize: 0,
+    discount: 0,
+    discountStep: 0,
+    url: "",
+    description: ""
   };
-  constructor(private active: ActivatedRoute, private servis: ProductsService, private router:Router) { }
+
+  constructor(
+    private active: ActivatedRoute,
+    private servis: ProductsService,
+    private router: Router
+  ) { }
+
   ngOnInit(): void {
     this.active.paramMap.subscribe(params => {
       this.id = params.get('id?');
       if (this.id != 0) {
         this.servis.GetProduct(this.id)
-          .subscribe((p:any) =>this.product = p);
+          .subscribe((p: any) => this.product = p);
       }
     })
   }
   switch() {
     this.hover = !this.hover;
   }
-  submit()
-  {    
-    if(this.id==0)
-      this.servis.CreateProduct(this.product).subscribe(r=>this.router.navigate(['/products']));
-    else 
-      this.servis.UpdateProduct(this.product,this.id).subscribe(r=>this.router.navigate(['/products']));
+  submit() {
+    if (this.id == 0)
+      this.servis.CreateProduct(this.product).subscribe(r => this.router.navigate(['/products']));
+    else
+      this.servis.UpdateProduct(this.product, this.id).subscribe(r => this.router.navigate(['/products']));
   }
+
+
 }
 
 
