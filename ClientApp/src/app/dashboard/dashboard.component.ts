24c749c527381a391b697bd5e66e1e644d6355aa
Xokhezi <110103053+Xokhezi@users.noreply.github.com>
2023-02-28 14:08:36 +0100

    logic for charts

--- ClientApp/src/app/dashboard/dashboard.component.ts
+++ ClientApp/src/app/dashboard/dashboard.component.ts
@@ -8,35 +8,60 @@ import { OrdersService } from '../services/orders.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  loading=true;
+  loading = true;
+  orders: any;
+  user: any;
 
-  orders:any;
-  user:any;
+  year: any;
+  totalThisMonth: any;
+  thisYearOrdersByMonths: any;
 
-  year:any;
-  totalThisMonth:any;
-  thisYearOrders:any;
-  
 
   constructor(public service: AuthService, private ordersService: OrdersService) { }
 
   //nutno dodělat query
   ngOnInit(): void {
-    this.year=0;
+    this.year = new Date().getFullYear();
     this.user = this.service.getcurrentUser();
     this.ordersService.GetOrders(this.year)
-    .subscribe((r:any)=>{
-      this.orders=r.filter((o:any)=>o.email==this.user.Email);
-      this.totalThisMonth=this.summarize(this.orders);
-      this.loading=false;
-    });
-    
+      .subscribe((r: any) => {
+        this.orders = r.filter((o: any) => o.email == this.user.Email);
+        this.totalThisMonth = this.summarize(this.orders);
+        this.loading = false;
+        this.thisYearOrdersByMonths=this.sortByMonth(this.orders, 2023)
+        console.log(this.thisYearOrdersByMonths);
+      });
+
   }
-  summarize(r:any) {
+  summarize(r: any) {
     let total = r.reduce((ac: any, currentPrize: { totalPrize: any; }) => {
-           return ac + currentPrize.totalPrize
+      return ac + currentPrize.totalPrize
     }, 0);
     return total;
+  }
+  sortByMonth(input: any, year: any) {
+    let monthValues = [];
+    for (let i = 0; i < 12; i++) {
+      let ordersForMonth = input.filter((o: any) => {
+        let placedOnDate = new Date(o.placedOn);
+        return placedOnDate.getFullYear() == year;
+      });
+       ordersForMonth = input.filter((o: any) => {
+        let placedOnDate = new Date(o.placedOn);
+        return placedOnDate.getMonth() == i;
+      });
+      let total = ordersForMonth.reduce((ac: any, currentPrize: { totalPrize: any; }) => {
+        return ac + currentPrize.totalPrize
+      }, 0);
+      monthValues.push(total);
+    }
+    return monthValues;
+  }
+  getBarHeight(value: number): number {
+    const min = Math.log10(100000);
+    const max = Math.log10(1000000);
+    const logValue = Math.log10(value);
+    return (logValue - min) / (max - min) * 100;
   }
 
 }
