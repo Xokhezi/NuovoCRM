44c31583a8f6cbff2f84d7b7f7a8f49fa424e3fa
Xokhezi <110103053+Xokhezi@users.noreply.github.com>
2023-03-04 17:03:56 +0100

    registration

--- ClientApp/src/app/registration/registration.component.ts
+++ ClientApp/src/app/registration/registration.component.ts
@@ -6,12 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
-  user:any
+  partner = {
+    id: 0,
+    name: "",
+    surname: "",
+    email: "",
+    phone: "",
+    position: "",
+    adress: "",
+    leadId: 0,
+    level: 0,
+    teamId: 0,
+    country: "",
+    userId: 0,
+    discountPrimary: 0,
+    discountSecundary: 0
+  };
+  user = {
+    id: 0,
+    email: "",
+    fullName: "",
+    password: "",
+    role: "",
+    country: "",
+    adress: "",
+    phone: "",
+    passwordRepeat: ""
+  };
+
   step:any;
   isEditable=false;
 
   ngOnInit(): void {
     this.step=1;
+  }
+  toDetail()
+  {
+    this.step=2;
   }
   register()
   {}
